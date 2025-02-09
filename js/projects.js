d90d4bc9599eb4613b91be7efa63ff3a9b610bd0
Your Name <pvidya770@gmail.com>
2025-02-09 14:38:39 +0530

    1st commit

---
+++ js/projects.js
@@ -0,0 +1,44 @@
+const projects = [{
+        img: "./assets/img/gmail.jpg",
+        name: "Gmail clone",
+        category: "fullstack",
+        techstack: ['html5', 'css-3', 'mongo', 'react-2', 'nodejs'],
+        frontend: "https://github.com/Vidya770/gmail-clone-electron-fronted-master",
+        backend: "https://github.com/Vidya770/gmail-clone-electron-backend-master",
+        deploymentUrl: "https://gmail-clone-4a3bd.netlify.app/account",
+        description: ` A Gmail clone that allows users to send emails, delete emails, star emails, and compose new emails..`
+    },
+    {
+        img: "./assets/img/bookmyhall.png",
+        name: "Book My Hall",
+        category: "backend",
+        techstack: ['html5', 'css-3', 'bootstrap-4', 'mongo', 'nodejs'],
+        backend: "https://github.com/Vidya770/Hall-booking",
+        deploymentUrl: "https://hall-booking-eta.vercel.app/",
+        description: `A Hall booking system developed the backend with the help of Node.JS and Express.Js`
+    },
+    {
+        img: "./assets/img/urlshortener.png",
+        name: "URL Shortener",
+        category: "backend",
+        techstack: ['html5', 'css-3', 'bootstrap-4', 'mongo', 'nodejs'],
+        frontend: "https://github.com/Vidya770/URL_Shortener_fronted",
+        backend: "https://github.com/Vidya770/URL_Shortener_backend",
+        deploymentUrl: "https://url-shortener-fronted.vercel.app/",
+        description: `A simple Web application for shortening the URLs, developed using Node.Js and Express.Js`
+    },
+    {
+        img: "./assets/img/mentors.jpg",
+        name: "My Mentors",
+        category: "frontend",
+        techstack: ['html5', 'css-3', 'mongo', 'nodejs'],
+        frontend: "https://github.com/Vidya770/crud_management/tree/main",
+        deploymentUrl: "https://crud-management-1.netlify.app/",
+        description: `A basic web application supporting all CRUD operations to map students to mentors.`
+    },
+    
+   
+
+]
+
+//module.exports.projects = projects;
---
+++ readme.md
@@ -0,0 +1,5 @@
+### Krishna Kireeti Portfolio
+
+# Link to my portfolio
+
+[https://krishnakiriti04.github.io/myportfolio/](https://krishnakiriti04.github.io/myportfolio/)
